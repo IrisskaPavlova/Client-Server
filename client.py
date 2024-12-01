e571d0ee49e936119828a57b4594f7eab01f5755
ir-ir-ir <kira0_05@mail.ru>
2024-12-01 15:07:01 +0300

    update: exception

--- client.py
+++ client.py
@@ -3,12 +3,32 @@ import socket as Socket
 from server import port, address,encoding
 
 socket = Socket.socket()
-socket.connect((address, port))
+
+# выполняем подключение к сокету на сервере
+try:
+	socket.connect((address, port))
+except OSError:
+	print("The server is not running!")
+	exit()
+
+print("\nThe connection to the server is established!")
+print("To complete the connection, send a 'q' request \n")
 
 while 1:
-	message = input("Client>")
-	if message == "q":
+
+	try:
+		message = input("Client>")
+		if message == "q":
+			print("\nThe connection is broken!")
+			break
+	except KeyboardInterrupt: # программа ждет input, а пользователь отключается не через q
+		print("\n\nThe connection is broken!")
 		break
-	socket.sendall(message.encode(encoding))
+
+	try:
+		socket.sendall(message.encode(encoding))
+	except OSError: # сервер больше не подключен
+		print("\nThe server is not running!")
+		break
 
 socket.close()
--- server.py
+++ server.py
@@ -1,29 +1,32 @@
 import socket as Socket
 
-address = "localhost"
+address = "localhost" # localhost гарантирует, что клиенты запущены только на локальном компьютере
 port = 801
-dataPackageSize = 1024
+dataPackageSize = 1024 # максимальный размер буфера в байтах
 encoding = "utf-8"
 
+# если пользователь отключился не через q
 def getDataPackage(connection):
-	try:
-		data = connection.recv(dataPackageSize)
-		return data
-	except ConnectionError:
-		return 0
-	
+    try:
+        data = connection.recv(dataPackageSize) # получение данных от client (здесь будет message)
+        return data
+    except ConnectionError:
+        return 0
+
 def startServer():
     print()
-    socket = Socket.socket()
-    socket.bind((address, port))
-    socket.listen()
-    connection, clientAddress = socket.accept()
-    print(f"{clientAddress} has connected")
+    socket = Socket.socket() # создаем объект сокета сервера
+    socket.bind((address, port)) # присваиваем экземпляру socket IP-адрес и номер порта
+    socket.listen() # переводим сервер в режим приёма соединений
+    connection, clientAddress = socket.accept() # получение входящих подключений:
+                                                # через conn сервер взаимодействует с клиентом
+                                                # address - адрес подключившегося клиента
+    print(f"{clientAddress}, has connected")
     while data := getDataPackage(connection):
-        data=data.decode(encoding)
+        data=data.decode(encoding) # декодируем набор байт
         print("Servser>",data[::-1])
-        connection.sendall(data.encode(encoding))
+        connection.sendall(data.encode(encoding)) # отправляем ответ client
     print(f"{clientAddress} has disconnected")
-	
+
 if __name__ == '__main__':
-	startServer()
+    startServer()
