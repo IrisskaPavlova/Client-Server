5371ee99d7111cf7fa5597d8bed4155e7529f0c6
anastasia <94540033+shepnst@users.noreply.github.com>
2024-12-01 22:55:49 +0300

    server.py updated

--- server.py
+++ server.py
@@ -1,42 +1,42 @@
 import socket as Socket
 
-address = "localhost" # localhost гарантирует, что клиенты запущены только на локальном компьютере
+address = "localhost" # localhost ensures that clients are running only on the local computer
 port = 801
-dataPackageSize = 1024 # максимальный размер буфера в байтах
+dataPackageSize = 1024 # max size of buffer in bytes 
 encoding = "utf-8"
 
-# если пользователь отключился не через q
+# if the user disconnected not using 'q'
 def getDataPackage(connection):
     try:
-        data = connection.recv(dataPackageSize) # получение данных от client (здесь будет message)
+        data = connection.recv(dataPackageSize) #receiving data from client (here will be message)
         return data
     except ConnectionError:
         return 0
 
 def startServer():
-    socket = Socket.socket() # создаем объект сокета сервера
+    socket = Socket.socket() # creating a server socket object
     socket.settimeout(60)
     try:
-        socket.bind((address, port)) # присваиваем экземпляру socket IP-адрес и номер порта
+        socket.bind((address, port)) #assign the socket instance an IP address and a port number
     except OSError:
         print("Port number", port, "is occupied")
         exit()
-    socket.listen() # переводим сервер в режим приёма соединений
+    socket.listen() # switching the server to connection acceptance mode(waiting for the message)
     while 1:
         try:
-            connection, clientAddress = socket.accept() # получение входящих подключений:
-                                                        # через conn сервер взаимодействует с клиентом
-                                                        # address - адрес подключившегося клиента
-        # Если функция accept() ожидает клиента дольше минуты - выбрасывается TimeoutException
+            connection, clientAddress = socket.accept() # receiving incoming connections
+                                                        # using  conn server communicating with client
+                                                        # address - address of connected client
+        # if function accept() if waiting for client more than 1 minute -  throw TimeoutException
         except TimeoutError:
             print("There have been no clients for 1 minute")
             exit()
 
         print(f"{clientAddress}, has connected")
         while data := getDataPackage(connection):
-            data=data.decode(encoding) # декодируем набор байт
+            data=data.decode(encoding) # decoding a set of bytes
             print("Servser>",data[::-1])
-            connection.sendall(data.encode(encoding)) # отправляем ответ client
+            connection.sendall(data.encode(encoding)) # send an answer to client
         print(f"{clientAddress} has disconnected")
 
 if __name__ == '__main__':
